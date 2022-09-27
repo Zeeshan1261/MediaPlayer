59499f50c612be37a389485095f8177f25dbf481
Zeeshan1261 <zk12697@gmail.com>
2022-09-27 16:43:02 +0530

    Media Player Created using React.Js

--- src/VoicePlayer.jsx
+++ src/VoicePlayer.jsx
@@ -1,26 +1,27 @@
-import { useRef } from "react"
-import { useState } from "react"
+import { useEffect, useRef, useState } from "react"
 import {GrSpotify} from 'react-icons/gr'
+import {GiPreviousButton} from 'react-icons/gi'
+import {GiNextButton} from 'react-icons/gi'
+import {BsPlayCircle} from 'react-icons/bs'
+import {BsPauseCircle} from 'react-icons/bs'
 function Voiceplayer() {
 const myAudio = [
+'./songs/sing.mp3',
 './songs/data.wav',
 './songs/test.wav',
 './songs/voice.mp3',
-'./songs/sing.mp3',
 './songs/song.mp3',
 ]
 const pictures = [
+'/pictures/song4.webp',
 '/pictures/song1.webp',
 '/pictures/song2.webp',
 '/pictures/song3.jpeg',
-'/pictures/song4.webp',
 '/pictures/song5.jpeg'
 ]
 const [increase,setincrease] = useState(0)
 const [song,setsong] = useState(myAudio[increase])
-const [data,setdata] = useState(pictures[increase])
-
-// const count = useRef(0)
+const [data,setdata] = useState(pictures[increase]) 
 console.log(song);   
 const add = ()=> {
 setincrease(increase+1)
@@ -33,14 +34,31 @@ setincrease(increase-1)
 setsong(myAudio[increase])
 setdata(pictures[increase])
 }   
+const myRef = useRef()
+const [playing,setplaying] = useState(false)
+
+useEffect(()=> {
+
+if(playing)  {
+myRef.current.play()
+    } 
+else{
+myRef.current.pause()
+    }
+})
+
 return(
 <>
-<img className="dataimg" src={data}/> <br></br> 
-<audio className="myaudio" controls
-source="true" src={song} type={'audio/mp3'}  /> <br></br>
+<img alt="" className="dataimg"src={data}/><br></br> 
+<audio className="myaudio" controls  
+src={song} ref={myRef}  type={'audio/mp3'}/><br></br>
+<div className="buttons">
 <h1 className="h1">Ubuntu Player<GrSpotify/></h1>
-<button onClick={()=> prev()} className="prev">Prev</button>
-<button onClick={()=> add()}  className="next">Next</button>
+<button onClick={()=> prev()} className="prev"><GiPreviousButton/></button>
+<button onClick={()=> setplaying(!playing)} className="play">{playing ? <BsPauseCircle/>:<BsPlayCircle/>}</button>
+{/* <button onClick={()=> pause()}className="pause"><FaPauseCircle/></button> */}
+<button onClick={()=> add()}  className="next"> <GiNextButton/></button>
+</div>
 </>
 )
 }
